d29a05587cdfe28932b4ac95367101f94f061621
Ligre <ligreman@users.noreply.github.com>
2015-05-10 23:11:33 +0200

    Fragmentos y modelos REST

--- app/src/main/java/com/ligresoftware/queechanenelcine/fragments/FavouriteFragment.java
+++ app/src/main/java/com/ligresoftware/queechanenelcine/fragments/FavouriteFragment.java
@@ -118,7 +118,6 @@ public class FavouriteFragment extends ListFragment {
      * fragment to allow an interaction in this fragment to be communicated
      * to the activity and potentially other fragments contained in that
      * activity.
-     * <p/>
      * See the Android Training lesson <a href=
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
